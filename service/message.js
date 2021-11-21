76b461f2599e5af30cf03c79bc17e143951bb1c8
suraj <suraj@litmusblox.io>
2021-11-21 15:40:44 +0530

    added send message api integration

--- service/message.js
+++ service/message.js
@@ -1,10 +1,37 @@
-const { message } = require('../db')
+const axios = require('axios')
+
+const {messages:messageDb}  = require('../db')
 
 const processIncomingMessage = (message)=>{
-    //todo 
+    //todo
+    messageDb.push(message)
+    console.log(message)
+    sendMessage(message.sender.id,{"text":"Hello!!"})
+}
 
+
+const sendMessage = async (senderId, response)=>{
+
+    const request_body = {
+        'recipient': {
+            'id': senderId
+        },
+        'message': response
+    }
+
+    try{
+
+        await axios.post('https://graph.facebook.com/v12.0/me/messages?access_token='+process.env.ACCESS_TOKEN,{
+            ...request_body
+        })
+
+    }catch(ex){
+        console.error(ex)
+    }
+   
 }
 
 module.exports = {
-    processIncomingMessage
+    processIncomingMessage,
+    callSendApi: sendMessage
 }
