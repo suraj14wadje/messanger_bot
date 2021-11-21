690e5d272e7e8ff8556978248d3c7f54a4dc5e88
suraj <suraj@litmusblox.io>
2021-11-21 20:27:39 +0530

    mock db added

--- db/index.js
+++ db/index.js
@@ -1,5 +1,32 @@
 
-let messages=[]
-let users = []
+const messages ={
+    get :function(){return this.data},
+    push:function(data){this.data.push(data)},
+    filter:function(cb){return this.data.filter(cb)},
+    find:function(cb){return this.data.find(cb)},
+    set:function(data){this.data = data},
+    delete:function(id){this.data = this.data.filter(m=>m.id!=id)},
+    data:[],
+}
+
+const users ={
+    get :function(){return this.data},
+    push:function(data){
+        this.data.push(data);
+        return data;
+    },
+    filter:function(cb){return this.data.filter(cb)},
+    find:function(cb){return this.data.find(cb)},
+    set:function(data){this.data = data},
+    delete:function(id){this.data = this.data.filter(m=>m.id!=id)},
+    update:function(user){
+        const newData = this.filter(u=>u.id!=user.id);
+        newData.push(user);
+        this.set(newData);
+        return newData;
+    },
+    data:[],
+}
+
 
 module.exports = { messages,users }
