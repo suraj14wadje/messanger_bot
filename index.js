8538ba7cee757d42d282e2606638d9d60a3c8883
suraj <suraj@litmusblox.io>
2021-11-25 20:11:45 +0530

    added mongodb setup

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose   = require('mongoose');
+
 const router = require('./routes')
 
-const app = express();
+const dbUrl = process.env.dbUrl 
+mongoose.connect(dbUrl)
+    .then(()=>console.log("connected to db!"))
+    .catch((error)=>console.log("Error connecting to db",error))
 
+const app = express();
 app.use(express.json())
 app.use(router);
-
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT , () => console.log(`Listening on port ${PORT}`));
