3a0aa82085ce3aebc0b6db50d60df378bdfd056a
suraj <suraj@litmusblox.io>
2021-11-26 14:33:07 +0530

    export server to be used in integration test

--- index.js
+++ index.js
@@ -11,4 +11,6 @@ app.use(express.json())
 app.use(router);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT , () => console.log(`Listening on port ${PORT}`));
+const server = app.listen(PORT , () => console.log(`Listening on port ${PORT}`));
+
+module.exports = server;
